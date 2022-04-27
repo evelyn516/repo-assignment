a1ab730d46c5f56a7280b5158d3f51b8d1c0328d
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-27 14:27:50 +0100

    Result page changes

--- repo-pro/src/components/Result/index.js
+++ repo-pro/src/components/Result/index.js
@@ -9,14 +9,14 @@ const Result = ({ username }) => {
         <div className="repo-data">
             { isLoading && <div>Loading...</div>}
             { repos && repos.map((result) => (
-                <div id={result.id}>
+                <div key={result.id}>
                 <h3>{result.name}</h3>
-                <p>{result.description}</p>
-                <p>forks = {result.forks_count}</p>
-                <p>watchers = {result.watchers_count}</p>
-                <p>created at {result.created_at.slice(0, 10)}</p>
-                <p>updated_at {result.updated_at.slice(0,10)}</p>
-                <p>clone here: <a href={result.clone_url}>{result.clone_url}</a></p>
+                <p>Description: {result.description}</p>
+                <p>Forks = {result.forks_count}</p>
+                <p>Watchers = {result.watchers_count}</p>
+                <p>Created at {result.created_at.slice(0, 10)}</p>
+                <p>Last updated_at {result.updated_at.slice(0,10)}</p>
+                <p>Clone here: <a href={result.clone_url}>{result.clone_url}</a></p>
             </div>
             ))}
         </div>
