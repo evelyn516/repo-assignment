77cbf3571c109eb149d165192d91795ee7f8ed98
Evie <evie.ball32@gmail.com>
2022-04-27 10:41:34 +0100

    working, basic setup achieved

--- repo-pro/src/App.js
+++ repo-pro/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
+import Header from './components/Header';
 
 function App() {
     return (
         <div id="app">
-            <main>
-                
-            </main>
+          <Header />
+            
         </div>
     )
 }
