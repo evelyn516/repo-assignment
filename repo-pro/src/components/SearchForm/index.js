c058384986af629bc523594f44fea2ea5c77a862
Evie <evie.ball32@gmail.com>
2022-04-27 10:58:55 +0100

    searchForm updates

--- repo-pro/src/components/SearchForm/index.js
+++ repo-pro/src/components/SearchForm/index.js
@@ -2,7 +2,23 @@ import React, { useState } from 'react';
 /* this getResult not linked to actions as it is a prop? */
 
 function SearchForm({ getResult }) {
+   
+    const handleSubmit = e => {
+    }
+    
+    const input = e.target.value {
 
+    }
+
+    return (
+        <>
+        <form>
+            <input id='searchEntry' type='text' placeholder='search user here' label="user"/>
+            <input id='formSubmitButton' type='submit' value="Search"/>
+        </form>
+        </>
+    )
 };
 
 export default SearchForm;
+
